09e23059eaaff51cc0eb752e26fc36f9f972045b
Gwen Eidsmoe <gwen.eidsmoe@gmail.com>
2026-01-22 15:37:02 -0500

    Added cpu.rs for CPU struct

---
+++ src/cpu.rs
@@ -0,0 +1,44 @@
+use crate::registers;
+
+pub fn two_bytes_to_u16(lsb: u8, msb: u8) -> u16 {
+    (((msb as u16) << 8) | (lsb as u16)).into()
+}
+  
+pub struct CPU {
+pub pc: u16,
+pub sp: u16,
+pub registers: registers::Registers,
+pub flags_register: registers::FlagsRegister,
+pub ram: [u8; 0x10000],
+}
+
+impl CPU {
+pub fn new() -> CPU {
+    return CPU { pc: 0x100, sp: 0xFFFF, registers: registers::Registers::new(), flags_register: registers::FlagsRegister::new(), ram: [0; 0x10000], }
+}
+
+pub fn pop(&mut self) -> u16 {
+    let lsb = self.ram[self.sp as usize];
+    let msb = self.ram[(self.sp + 1) as usize];
+    self.sp += 2;
+    return two_bytes_to_u16(lsb, msb);
+}
+
+pub fn push(&mut self, next_address: u16) {
+    let lsb: u8 =  (next_address & 0xFF) as u8;
+    let msb: u8 = ((next_address & 0xFF00) >> 8) as u8;
+    self.ram[(self.sp - 1) as usize] = msb;
+    self.ram[(self.sp - 2) as usize] = lsb;
+    self.sp -= 2;
+}
+
+pub fn read_u16_at(&self, addr: u16) -> u16 {
+    let lsb = self.ram[addr as usize];
+    let msb = self.ram[(addr + 1) as usize];
+    two_bytes_to_u16(lsb, msb)
+}
+
+pub fn read_u16_at_pc(&self) -> u16 {
+    self.read_u16_at(self.pc)
+}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,15 @@
 mod registers;
-pub mod tests;  
+mod cpu;
+pub mod tests;
 
 use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-
-
-fn two_bytes_to_u16(lsb: u8, msb: u8) -> u16 {
-  (((msb as u16) << 8) | (lsb as u16)).into()
-}
-
-pub struct CPU {
-  pub pc: u16,
-  pub sp: u16,
-  pub registers: registers::Registers,
-  pub flags_register: registers::FlagsRegister,
-  pub ram: [u8; 0x10000],
-}
-
-impl CPU {
-  pub fn new() -> CPU {
-    return CPU { pc: 0x100, sp: 0xFFFF, registers: registers::Registers::new(), flags_register: registers::FlagsRegister::new(), ram: [0; 0x10000], }
-  }
-
-  pub fn pop(&mut self) -> u16 {
-    let lsb = self.ram[self.sp as usize];
-    let msb = self.ram[(self.sp + 1) as usize];
-    self.sp += 2;
-    return two_bytes_to_u16(lsb, msb);
-  }
-
-  pub fn push(&mut self, next_address: u16) {
-    let lsb: u8 =  (next_address & 0xFF) as u8;
-    let msb: u8 = ((next_address & 0xFF00) >> 8) as u8;
-    self.ram[(self.sp - 1) as usize] = msb;
-    self.ram[(self.sp - 2) as usize] = lsb;
-    self.sp -= 2;
-  }
-
-  pub fn read_u16_at(&self, addr: u16) -> u16 {
-    let lsb = self.ram[addr as usize];
-    let msb = self.ram[(addr + 1) as usize];
-    two_bytes_to_u16(lsb, msb)
-  }
-
-  pub fn read_u16_at_pc(&self) -> u16 {
-    self.read_u16_at(self.pc)
-  }
-}
-
 use serde::{Deserialize, Serialize};
+use cpu::{CPU, two_bytes_to_u16};
+
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Instruction {
@@ -89,9 +47,8 @@ pub fn execute_opcode(instruction_set: &InstructionSet, cpu: &mut CPU) {
   let opcode = cpu.ram[cpu.pc as usize];
     cpu.pc = cpu.pc.wrapping_add(1); // increment PC past the opcode
     match opcode  {
-      0x00 => {
-        //println!("{:#04X}: NOP", opcode);
-      }
+      0x00 => { /* NO OP */ }
+      0x10 => { /* TODO IMPLEMENT STOIP */}
       /* START LOAD OPCODES */
       /* Start direct loads */
       0x01 => {
@@ -358,7 +315,6 @@ pub fn execute_opcode(instruction_set: &InstructionSet, cpu: &mut CPU) {
           let lsb = cpu.ram[cpu.pc as usize];
           let msb = cpu.ram[(cpu.pc + 1) as usize];
           let next_address: u16 = two_bytes_to_u16(lsb, msb);
-          
           // push current PC onto the stack
           cpu.push(cpu.pc + 2);
           // set the PC to be A16
